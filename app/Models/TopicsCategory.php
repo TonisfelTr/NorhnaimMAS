c570cb2df5181223324421c8c04d4923444b1490
Ilya <seintte@gmail.com>
2024-11-30 22:33:49 +0700

    Поправка к блогу

--- app/Models/TopicsCategory.php
+++ app/Models/TopicsCategory.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class TopicsCategories extends Model
+class TopicsCategory extends Model
 {
+    use HasFactory;
+
+    protected $guarded = [];
+
     public function topics(): BelongsTo {
         return $this->belongsTo(Topic::class);
     }
